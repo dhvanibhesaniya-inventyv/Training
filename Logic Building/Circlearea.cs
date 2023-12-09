d2f6cc3ae5e11dea68da26596b830e3b83062d2f
DhvaniBhesaniya09 <153286337+DhvaniBhesaniya09@users.noreply.github.com>
2023-12-09 11:29:55 +0530

    Add files via upload

---
+++ Logic Building/Circlearea.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void CircleArea(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle: ");
+            float radius = float.Parse(Console.ReadLine());
+
+            float area = CalculateArea(radius);
+            Console.WriteLine($"The area of the circle is {area}.");
+        }
+
+        static float CalculateArea(float radius)
+        {
+            return (float)(Math.PI * radius * radius);
+        }
+    }
+}
